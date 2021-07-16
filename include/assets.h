a07bea772b0afccd5ad5c21f01c6295c8bf303d4
Denis <Dysprosium92@ya.ru>
2021-07-16 12:45:01 +0300

    Improved assets class

--- include/assets.h
+++ include/assets.h
@@ -2,6 +2,8 @@
 #define FIFTEEN_PUZZLE_GAME_INCLUDE_SETTINGS_H_
 
 #include <SFML/Graphics.hpp>
+#include <string>
+#include <exception>
 
 class AssetsSingleton final {
 
@@ -11,6 +13,15 @@ class AssetsSingleton final {
     return settings;
   }
 
+  sf::Font LoadFont(std::string fontName) {
+    sf::Font font;
+    if (!font.loadFromFile(fontName))
+      throw std::runtime_error("Could not open file");
+    return font;
+  }
+
+  AssetsSingleton() = default;
+  ~AssetsSingleton() = default;
   AssetsSingleton(const AssetsSingleton &) = delete;
   AssetsSingleton &operator=(const AssetsSingleton &) = delete;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include <assets.h>
 #include "spdlog/spdlog.h"
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
-
-  while (window.isOpen())
-  {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
-
-  spdlog::info("Welcome to spdlog!");
-
   return 0;
 }

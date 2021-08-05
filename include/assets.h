e04dc871a852554e62e111586234caf862672012
Denis <Dysprosium92@ya.ru>
2021-08-05 18:05:47 +0300

    Refactored LoadFont function

--- include/assets.h
+++ include/assets.h
@@ -7,30 +7,11 @@
 
 namespace Game {
 
-class AssetsSingleton final {
-
- public:
-  static AssetsSingleton &Instance() {
-    static AssetsSingleton settings;
-    return settings;
-  }
-
-  sf::Font LoadFont(std::string fontName) {
-    sf::Font font;
-    if (!font.loadFromFile(fontName))
-      throw std::runtime_error("Could not open file");
-    return font;
-  }
-
-  AssetsSingleton() = default;
-  ~AssetsSingleton() = default;
-
-  AssetsSingleton(const AssetsSingleton &) = delete;
-  AssetsSingleton &operator=(const AssetsSingleton &) = delete;
-
-  AssetsSingleton(const AssetsSingleton &&) = delete;
-  AssetsSingleton &operator=(const AssetsSingleton &&) = delete;
-};
+sf::Font LoadFont(std::string fontName) {
+  sf::Font font;
+  if (!font.loadFromFile(fontName))
+    throw std::runtime_error("Could not open file");
+  return font;
 }
-
+}
 #endif //FIFTEEN_PUZZLE_GAME_INCLUDE_SETTINGS_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
-#include "log.h"
 
-int main()
-{
+#include "log.h"
+#include "assets.h"
+
+int main() {
   ::Game::Log::Init();
   GAME_WARN("Initialized log!");
 
+  try {
+    auto font = Game::LoadFont("../resources/calibri.ttf");
+  }
+  catch (...) {
+    GAME_CRITICAL("Font could not find!");
+  }
   return 0;
 }

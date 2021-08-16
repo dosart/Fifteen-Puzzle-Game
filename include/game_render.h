52681dad62525a7ce3d19963ba441e1890336294
Denis <Dysprosium92@ya.ru>
2021-08-16 21:18:43 +0300

    Added render

--- include/game_render.h
+++ include/game_render.h
@@ -1,12 +1,30 @@
-//
-// Created by dosart on 8/10/21.
-//
-
 #ifndef FIFTEEN_PUZZLE_GAME_SRC_GAME_RENDER_H_
 #define FIFTEEN_PUZZLE_GAME_SRC_GAME_RENDER_H_
 
-class game_render {
+#include <SFML/Graphics.hpp>
+#include "fifteen_puzzle_game.h"
 
+namespace Game {
+
+class GameRender : public sf::Drawable, public sf::Transformable {
+ public:
+  GameRender(FifteenPuzzleGame *game, sf::Font font);
+  ~GameRender() = default;
+
+  bool Init();
+  void Render();
+  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+ private:
+  FifteenPuzzleGame *_game;
+  sf::Font _font;
+
+  sf::RenderWindow _window;
+  sf::Text _text;
+
+  const float kFieldSize = 500.0;
+  const float kCellSize = 120.0;
 };
+}
 
 #endif //FIFTEEN_PUZZLE_GAME_SRC_GAME_RENDER_H_
--- src/game_render.cpp
+++ src/game_render.cpp
@@ -1,5 +1,68 @@
-//
-// Created by dosart on 8/10/21.
-//
 
 #include "game_render.h"
+
+Game::GameRender::GameRender(Game::FifteenPuzzleGame *game, sf::Font font)
+    : _game{game}, _font{font} {
+
+}
+
+bool Game::GameRender::Init() {
+  setPosition(50.f, 50.f);
+  _window.create(sf::VideoMode(600, 600), "Fifteen-Puzzle-Game");
+  _window.setFramerateLimit(60);
+  _text = sf::Text("F2 - New Game / Esc - Exit / Arrow Keys - Move Tile", _font, 20);
+  _text.setFillColor(sf::Color::Cyan);
+  _text.setPosition(5.f, 5.f);
+  return true;
+}
+
+void Game::GameRender::Render() {
+  _window.clear();
+  _window.draw(*this);
+  _window.draw(_text);
+  _window.display();
+}
+
+void Game::GameRender::draw(sf::RenderTarget &target, sf::RenderStates states) const {
+
+  states.transform *= getTransform();
+  sf::Color color = sf::Color(200, 100, 200);
+
+  sf::RectangleShape frameOfBoard(sf::Vector2f(kFieldSize, kFieldSize));
+  frameOfBoard.setOutlineThickness(2.f);
+  frameOfBoard.setOutlineColor(color);
+  frameOfBoard.setFillColor(sf::Color::Transparent);
+  target.draw(frameOfBoard, states);
+
+  sf::RectangleShape cellOfBoard(sf::Vector2f(kCellSize, kCellSize));
+  cellOfBoard.setOutlineThickness(2.f);
+  cellOfBoard.setOutlineColor(color);
+  cellOfBoard.setFillColor(sf::Color::Transparent);
+
+  sf::Text cellContents("", _font, 52);
+
+  auto &board = _game->GetBoard();
+  for (auto row = 0; row < board.GetRowCount(); ++row) {
+    frameOfBoard.setOutlineColor(color);
+    cellContents.setFillColor(color);
+    for (auto column = 0; column < board.GetColumnCount(); ++column) {
+      auto numberAsString = std::to_string(board.at(row, column));
+      cellContents.setString(numberAsString);
+
+      if (board.isElementCorrect(row, column))
+        cellContents.setFillColor(sf::Color::Green);
+
+      if (board.isNotEmpty(row, column)) {
+        float float_column = static_cast<float>(column);
+        float float_row = static_cast<float>(row);
+
+        sf::Vector2f position(float_column*kCellSize + 10.f, float_row*kCellSize + 10.f);
+        cellOfBoard.setPosition(position);
+        cellOfBoard.setPosition(position.x + 30.f + (board.at(row, column) < 10 ? 15.f : 0.f), position.y + 25.f);
+        target.draw(cellOfBoard, states);
+        target.draw(cellOfBoard, states);
+      }
+
+    }
+  }
+}

fa980d9c823e8a9d0cfba0bf0675c877c6fde1ac
Denis <Dysprosium92@ya.ru>
2021-08-22 19:07:56 +0300

    Added tests for assets

--- test/game_test/test_assets.cpp
+++ test/game_test/test_assets.cpp
@@ -1,9 +1,14 @@
 #include <gtest/gtest.h>
+#include <assets.h>
+#include <optional>
 
-TEST(SimpleTest, SimpleTestName) {
-  int x = 0;
-  int y = 0;
+TEST(AssertTest, LoadFontWithException) {
+  auto font = Game::LoadFont("Bad");
+  ASSERT_EQ(font, std::nullopt);
+}
 
-  ASSERT_EQ(x, y);
+TEST(AssertTest, LoadFontWithoutException) {
+  auto font = Game::LoadFont("../../resources/calibri.ttf");
+  ASSERT_EQ(font.has_value(), true);
 }
 

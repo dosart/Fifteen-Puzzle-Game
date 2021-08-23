b7af044a2d95e8225265f644a85f3d8b2005193a
Denis <Dysprosium92@ya.ru>
2021-08-23 20:15:02 +0300

    Added tests for Convert2DIndexTo1DIndex function

--- test/game_test/test_utils.cpp
+++ test/game_test/test_utils.cpp
@@ -1,4 +1,9 @@
-//
-// Created by dosart on 8/23/21.
-//
+#include <gtest/gtest.h>
+#include "utils/converter.h"
 
+TEST(Convert2DIndex, SimpleTest)
+{
+  ASSERT_EQ(Utils::Convert2DIndexTo1DIndex(1,2,4), 6);
+  ASSERT_EQ(Utils::Convert2DIndexTo1DIndex(0,0,4), 0);
+  ASSERT_EQ(Utils::Convert2DIndexTo1DIndex(3,3,4), 15);
+}

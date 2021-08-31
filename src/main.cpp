30a458b5767d2f66ca51e67609e1468c9d5a597b
Denis Bolshakov <Dysprosium92@ya.ru>
2021-08-31 10:17:36 +0300

    Added logs

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,24 @@
 
 int main() {
   ::Game::Log::Init();
-  GAME_WARN("Initialized log!");
+  GAME_INFO("Initialized log!");
 
   if (auto font = Game::LoadFont("../resources/calibri.ttf"); font.has_value()) {
+      GAME_INFO("Font loadded.");
+    
     ::Game::FifteenPuzzleGame game;
+     GAME_INFO("Game created.");
+    
     ::Game::GameRender render(&game, font.value());
+     GAME_INFO("Render created.");
+    
     ::Game::GameController controller(&game, &render);
+    GAME_INFO("Controller created.");
+    
+    GAME_INFO("Game runing.");
     controller.Run();
+    GAME_INFO("Game finished.");
+    
   } else
     GAME_CRITICAL("Font could not find!");
 
